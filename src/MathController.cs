a6bba5016302ccbec494be58376dae5eb942f21a
Khemais Menzli <kmenzli@exoplatform.com>
2019-01-02 00:11:44 +0100

    Create a Dockerfile sample

---
+++ src/MathController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace MathAPI
+{
+    [Route("api/[controller]")]
+    public class MathController : Controller
+    {
+        [HttpGet("Ajouter")]
+        public int GetAddition(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
